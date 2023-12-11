3a204afdaf4f75d6f43878a87db78421efde261e
Yuxin Wei <71242829+YushinWei@users.noreply.github.com>
2023-12-10 18:36:33 -0600

    Add files via upload

--- visual5/historical_distribution.R
+++ visual5/historical_distribution.R
@@ -2,15 +2,11 @@ library(shiny)
 
 ui <- fluidPage(
   titlePanel("genus Frasera historical distribution modeling"),
-  sliderInput("kyr", "thousands years ago", min = -18, max = 0, value = 0, step = 3),
-  imageOutput("Distribution"),
-  textOutput("name")
+  sliderInput("kyr", "thousands years ago", min = -18, max = 0, value = 0, step = 3, animate = TRUE),
+  imageOutput("Distribution")
 )
 
 server <- function(input, output) {
-  output$name <- renderText({
-    paste0("C:/Users/75180/679visualization/grouppj/maxent/", (input$kyr + 18) / 3 + 1, ".png")
-  })
   
   output$Distribution <- renderImage({
     filename <- normalizePath(file.path(paste0("C:/Users/75180/679visualization/grouppj/maxent/", (input$kyr + 18) / 3 + 1, ".png")))
